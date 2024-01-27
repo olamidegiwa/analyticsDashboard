f56c4cbc1e1f194e1ff4a3679b25d9dbd7d69e75
olamidegiwa <giwasodiq7@gmail.com>
2024-01-27 14:53:13 +0100

    aos updated

--- src/component/LastOrder/LastOrder.jsx
+++ src/component/LastOrder/LastOrder.jsx
@@ -125,7 +125,7 @@ const LastOrder = () => {
         </table>
       </div>
 
-      <div className=" border-2 rounded-xl p-3 topplatform dark:bg-black mb-4 ">
+      <div className=" border-2 rounded-xl p-3 topplatform dark:bg-black mb-4 px-1 ">
         <div data-aos="zoom-out-right" className="flex justify-between mb-2 ">
           <p className="fs-4 fw-semibold text-gray-900 dark:text-pink-500 ">
             Top Platform
@@ -135,19 +135,19 @@ const LastOrder = () => {
 
         <div className="flex flex-col gap-2 py-2  ">
           <p
-            // data-aos="zoom-out-down"
+           data-aos="zoom-in-up"
             className="text-gray-900 fs-4 fw-semibold dark:text-white"
           >
             Book Bazaar
           </p>
           <div
-            // data-aos="zoom-out-left"
+            data-aos="zoom-in-up"
             className="w-100 h-[16px] rounded-full bg-gray-300 dark:bg-white"
           >
             <div className="w-50 h-[16px] bg-blue-700 rounded-full"></div>
           </div>
           <div
-            // data-aos="zoom-out-right"
+            data-aos="zoom-in-up"
             className="flex items-center justify-between fs-4 text-gray-400 dark:text-white"
           >
             <p>$2,500,000</p>
@@ -157,19 +157,19 @@ const LastOrder = () => {
 
         <div className="flex flex-col gap-2 py-2">
           <p
-            // data-aos="zoom-out-down"
+            data-aos="zoom-in-up"
             className="text-gray-900 fs-4 fw-semibold dark:text-white"
           >
             Artisan Aisle
           </p>
           <div
-            // data-aos="zoom-out-left"
+            data-aos="zoom-in-up"
             className="w-100 h-[16px] rounded-full bg-gray-300 dark:bg-white"
           >
             <div className="h-[16px] bg-blue-400 rounded-full Aisle"></div>
           </div>
           <div
-            // data-aos="zoom-out-right"
+            data-aos="zoom-in-up"
             className="flex items-center justify-between fs-4 text-gray-400 dark:text-white"
           >
             <p>$1,800,000</p>
@@ -178,19 +178,19 @@ const LastOrder = () => {
         </div>
         <div className="flex flex-col gap-2 py-2">
           <p
-            // data-aos="zoom-out-down"
+           data-aos="zoom-in-up"
             className="text-gray-900 fs-4 fw-semibold dark:text-white"
           >
             Toy Troop
           </p>
           <div
-            // data-aos="zoom-out-left"
+            data-aos="zoom-in-up"
             className="w-100 h-[16px] rounded-full bg-gray-300 dark:bg-white"
           >
             <div className="h-[16px] Troop bg-yellow-500 rounded-full"></div>
           </div>
           <div
-            // data-aos="zoom-out-right"
+            data-aos="zoom-in-up"
             className="flex items-center justify-between fs-4 text-gray-400 dark:text-white"
           >
             <p>$1,200,000</p>
